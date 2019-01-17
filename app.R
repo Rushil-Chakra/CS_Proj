87651337506041590e41bf3d97813aa6b9630090
Rushil-Chakra <44096041+Rushil-Chakra@users.noreply.github.com>
2019-01-17 00:15:42 -0800

    Update app.R

--- app.R
+++ app.R
@@ -23,35 +23,38 @@ dat <- (xmlParse("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%2
 #dat <- xmlToList(data)
 #dat <- dat[1]
 #set up time
-currentTime <- Sys.time() %>% substring(12,16) %>% strsplit(split=':')
-currTime <- as.numeric(currentTime[[1]][[1]])+as.numeric(currentTime[[1]][[2]])/60
-sunrise <- gsub(":"," ",dat$results$channel$astronomy[1]) %>% strsplit(split=" ")
-sunrise <- sunrise[[1]] %>% as.vector()
-sunset <- gsub(":"," ",dat$results$channel$astronomy[2]) %>% strsplit(split=" ")
-sunset <- sunset[[1]] %>% as.vector()
+currentTime <- Sys.time() %>% substring(12,16) %>% strsplit(split = ':')
+currTime <- as.numeric(currentTime[[1]][[1]]) + as.numeric(currentTime[[1]][[2]])/60
+#Not sure if the as.vector part is needed
+sunrise <- (gsub(":"," ", dat$results$channel$astronomy[1]) %>% strsplit(split=" "))[[1]] %>% as.vector()
+#sunrise <- sunrise[[1]] %>% as.vector()
+sunset <- (gsub(":", " ", dat$results$channel$astronomy[2]) %>% strsplit(split=" "))[[1]] %>% as.vector()
+#sunset <- sunset[[1]] %>% as.vector()
 #returns military conversion of time
 military <- function(x){
-    if(x[3]=="pm"){
+    if(x[3] == "pm"){
         hours <- 12
     }
     else{
         hours <- 0
     }
-    return(hours+as.numeric(x[1])+as.numeric(x[2])/60)
+    return(hours + as.numeric(x[1]) + as.numeric(x[2])/60)
 }
 #set up weather continued
 sunlight <- military(sunrise)
 moonlight <- military(sunset)
+#I  have no idea what this is supposed to be
 moonx <- ((moonlight + (24 - moonlight + sunlight) %% 24)/24)/12
 temperature <- dat$results$channel$item$condition[3]
 weather <- dat$results$channel$item$condition[4]
 windSpeed <- dat$results$channel$wind[3]
-#get wind direction into letters
-dir <- round(as.numeric(dat$results$channel$wind[[2]])/45)
-directions <- c('North',"North East","East","South East","South","South West","West","North West")
+#get wind direction into cardinal directions
+#dir <- round(as.numeric(dat$results$channel$wind[[2]])/45)
+dir <- (dat$results$channel$wind[[2]] %>% as.numeric())/45 %>% round()
+directions <- c("North", "North East", "East", "South East", "South", "South West", "West", "North West")
 forecast <- list()
 for(i in 7:16){
-    forecast <- append(forecast,dat[[1]][[1]][[13]][i])
+    forecast <- append(forecast, dat[[1]][[1]][[13]][i])
 }
 #background setup
 color <- 'yellow'
@@ -59,12 +62,12 @@ if((currTime < sunlight && currTime > 0) || (currTime > moonlight && currTime <2
     color<- 'purple'
 }
 #stock setup #last price, change, change %
-SPY <- c(getQuote('SPY')[[2]],getQuote('SPY')[[3]],getQuote('SPY')[[4]])
-AMD <- c(getQuote('AMD')[[2]],getQuote('AMD')[[3]],getQuote('AMD')[[4]])
-NVDA <- c(getQuote('NVDA')[[2]],getQuote('NVDA')[[3]],getQuote('NVDA')[[4]])
-SOXL <- c(getQuote('SOXL')[[2]],getQuote('SOXL')[[3]],getQuote('SOXL')[[4]])
+SPY <- c(getQuote('SPY')[[2]], getQuote('SPY')[[3]], getQuote('SPY')[[4]])
+AMD <- c(getQuote('AMD')[[2]], getQuote('AMD')[[3]], getQuote('AMD')[[4]])
+NVDA <- c(getQuote('NVDA')[[2]], getQuote('NVDA')[[3]], getQuote('NVDA')[[4]])
+SOXL <- c(getQuote('SOXL')[[2]], getQuote('SOXL')[[3]], getQuote('SOXL')[[4]])
 #list of stocks to show as portfolio
-stocks <- list(SPY,AMD,NVDA,SOXL)
+stocks <- list(SPY, AMD, NVDA, SOXL)
 
 #changes color based on whether markets open and making money or losing money
 stockColor <- function(x){
@@ -87,7 +90,7 @@ moisture <- read.csv('~/Desktop/test/codes/Home_mon/Moisture-Sensor-master/data_
 moist_text <- "Healthy"
 moist_color <- 'green'
 moist_reading <- moisture[length(moisture[,1]),3]
-#threshold for how moist the water should be, higher value = dryer, rn its at 25%
+#threshold for how moist the water should be, higher value = dryer, right now its at 25%
 if(moist_reading <= 768){
     moist_text <- 'Danger'
     moist_color <- 'red'
@@ -96,7 +99,7 @@ if(moist_reading <= 768){
 gPalette <- c("#5AB953", "#71D161")
 
 ui <- dashboardPage(skin = color,
-    dashboardHeader(title = paste0(temperature,"˚ F ",weather)),
+    dashboardHeader(title = paste0(temperature,"˚ F ", weather)),
     dashboardSidebar(
         #creates sidebar stuff
         sidebarMenu( 
@@ -115,15 +118,16 @@ ui <- dashboardPage(skin = color,
                     htmlOutput('frame')
                 ),
                #stock portfolio
+               #figure out how to make this into a for loop, use a function, i is input output is vector of stock
                 fluidRow(
                     box(
                         textOutput('prices'),style=paste('background-color: ',stockBackground),
                         tags$table(style=paste("width: 100%; color:", stockText), 
                             tags$tr(tags$th("Ticker"), tags$th("Last Price"), tags$th("Change"), tags$th("Change %")),
-                            tags$tr(style=paste("color: ", stockColor(stocks[[1]])), tags$th("SPY"), tags$td(stocks[[1]][[1]]), tags$td(stocks[[1]][[2]]), tags$td(stocks[[1]][[3]])),
-                            tags$tr(style=paste("color: ", stockColor(stocks[[2]])), tags$th("AMD"), tags$td(stocks[[2]][[1]]), tags$td(stocks[[2]][[2]]), tags$td(stocks[[2]][[3]])),
-                            tags$tr(style=paste("color: ", stockColor(stocks[[3]])), tags$th("NVDA"), tags$td(stocks[[3]][[1]]), tags$td(stocks[[3]][[2]]), tags$td(stocks[[3]][[3]])),
-                            tags$tr(style=paste("color: ", stockColor(stocks[[4]])), tags$th("SOXL"), tags$td(stocks[[4]][[1]]), tags$td(stocks[[4]][[2]]), tags$td(stocks[[4]][[3]]))
+                            tags$tr(style = paste("color: ", stockColor(stocks[[1]])), tags$th("SPY"), tags$td(stocks[[1]][[1]]), tags$td(stocks[[1]][[2]]), tags$td(stocks[[1]][[3]])),
+                            tags$tr(style = paste("color: ", stockColor(stocks[[2]])), tags$th("AMD"), tags$td(stocks[[2]][[1]]), tags$td(stocks[[2]][[2]]), tags$td(stocks[[2]][[3]])),
+                            tags$tr(style = paste("color: ", stockColor(stocks[[3]])), tags$th("NVDA"), tags$td(stocks[[3]][[1]]), tags$td(stocks[[3]][[2]]), tags$td(stocks[[3]][[3]])),
+                            tags$tr(style = paste("color: ", stockColor(stocks[[4]])), tags$th("SOXL"), tags$td(stocks[[4]][[1]]), tags$td(stocks[[4]][[2]]), tags$td(stocks[[4]][[3]]))
                         )
                     ),
                     #wind speed
@@ -281,19 +285,19 @@ server <- function(input, output) {
     output$timeplot <- renderPlot({
         hourUpdate()
         ggplot()+
-            geom_rect(aes(xmin = 0, xmax = 1, ymin = 0, ymax = sunlight),fill='midnightblue',color='midnightblue')+
-            geom_rect(aes(xmin = 0, xmax = 1, ymin = sunlight, ymax = moonlight),fill='gold',color='gold')+
-            geom_rect(aes(xmin = 0,xmax = 1,ymin = moonlight, ymax = 24),fill='midnightblue',color='midnightblue')+
-            geom_segment(aes(x = 0,y = currTime,yend = currTime, xend = 1),color='green')+
+            geom_rect(aes(xmin = 0, xmax = 1, ymin = 0, ymax = sunlight), fill = 'midnightblue', color = 'midnightblue')+
+            geom_rect(aes(xmin = 0, xmax = 1, ymin = sunlight, ymax = moonlight), fill = 'gold', color = 'gold')+
+            geom_rect(aes(xmin = 0, xmax = 1, ymin = moonlight, ymax = 24), fill = 'midnightblue', color = 'midnightblue')+
+            geom_segment(aes(x = 0, y = currTime, yend = currTime, xend = 1), color = 'green')+
             coord_polar(theta = 'y', start = 2 * pi - currTime/12 * pi)+
-            ggtitle(paste0("Sunrise: ",dat$results$channel$astronomy[[1]]," Sunset: ",dat$results$channel$astronomy[[2]]))+
+            ggtitle(paste0("Sunrise: ", dat$results$channel$astronomy[[1]], " Sunset: ", dat$results$channel$astronomy[[2]]))+
             scale_y_continuous(breaks = seq(0,24,6))+
             theme(
                 axis.title = element_blank(),
                 axis.ticks = element_blank(),
                 axis.text.y = element_blank(),
                 line = element_blank(),
-                panel.background= element_blank(),
+                panel.background = element_blank(),
                 plot.title = element_text(
                     family = 'Trebuchet MS',
                     size = 32,
@@ -308,9 +312,9 @@ server <- function(input, output) {
         #set up moisture
         moisture <- read.csv('~/Desktop/test/codes/Home_mon/Moisture-Sensor-master/data_log.csv')
         moisture.new <- moisture[seq(1, nrow(moisture), 5),]
-        ggplot(moisture.new,aes(x = Time, y = Reading, group = Bed,color = factor(Bed)))+
+        ggplot(moisture.new, aes(x = Time, y = Reading, group = Bed, color = factor(Bed)))+
         geom_line() +
-        geom_hline(yintercept = 768,color = 'red', linetype = 'dashed') +
+        geom_hline(yintercept = 768, color = 'red', linetype = 'dashed') +
         labs(color = 'Pi Number') +
         scale_color_manual(values = gPalette) +
         theme_bw() +
